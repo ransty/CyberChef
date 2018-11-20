59430f178760a42d2df1d3cf54b063407c50bdc0
keano <keano.porcaro@gmail.com>
2018-11-20 15:09:41 +1100

    Added more functionality, need to validate input now

--- src/core/operations/Cron.mjs
+++ src/core/operations/Cron.mjs
@@ -40,15 +40,22 @@ class Cron extends Operation {
      */
     run(input, args) {
         const statement = args[0];
-        // Parameters to use in statement
-        // every, day-of-week, day-of-month, on, in,
+
         var inputArray = input.toLowerCase().split(" ");
+
+	// work out how to stop anything but *, number, , and -
+	//for (var j = 0; j < inputArray.length; j++) {
+	//	if (/^\d+$/.test(inputArray[j]) == false) {
+	//		return "That is a NOT a number";
+	//	}
+	//}
 
 	var returnStatement = "At";
 
-        if (inputArray.length < 5 || inputArray.length > 6)
+        if (inputArray.length < 5 || inputArray.length > 5)
 		return "not a valid length for a CRON expression";
 	
+	// Minute Controller
 	var minute = inputArray[0];
 	if (minute != "*")
 		if (parseInt(minute) > 59 || parseInt(minute) < 0)
@@ -56,6 +63,7 @@ class Cron extends Operation {
 		else if (minute.length < 2)
 			minute = "0" + minute;
         
+	// Hour Controller
 	var hour = inputArray[1];
 	if (hour != "*")
 		if (parseInt(hour) > 23 || parseInt(hour) < 0)
@@ -70,6 +78,70 @@ class Cron extends Operation {
 		hourMinute = "every minute past hour " + hour;
 
 	returnStatement = returnStatement + " " + hourMinute;
+
+	// Day of Month Controller
+	var dayOfMonth = inputArray[2];
+	var dayOfMonthStatement = "";
+	if (dayOfMonth != "*") {
+		if (parseInt(dayOfMonth) > 31 || parseInt(dayOfMonth) < 1) { 
+			return "day-of-month is not valid, please use between 1 and 31 or *";
+		}
+		dayOfMonthStatement = "on day-of-month " + dayOfMonth;
+	}
+
+	if (dayOfMonthStatement != "")
+		returnStatement = returnStatement + " " + dayOfMonthStatement;
+		
+	// Month controller
+	var month = inputArray[3];
+	var monthStatement = "";
+	if (month != "*")
+		if (parseInt(month) > 12 || parseInt(month) < 1)
+			return "month is not valid, please use between 1 and 12 or *";
+		monthStatement = "in ";
+		switch(parseInt(month)) {
+    		case 1:
+	        	monthStatement = monthStatement + "January";
+        		break;
+	    	case 2:
+	        	monthStatement = monthStatement + "February";
+	        	break;
+	    	case 3:
+	        	monthStatement = monthStatement + "March";
+	        	break;
+	    	case 4:
+	        	monthStatement = monthStatement + "April";
+	        	break;
+	    	case 5:
+	        	monthStatement = monthStatement + "May";
+	        	break;
+	    	case 6:
+	        	monthStatement = monthStatement + "June";
+	        	break;
+	    	case 7:
+	        	monthStatement = monthStatement + "July";
+	        	break;
+		case 8:
+			monthStatement = monthStatement + "August";
+			break;
+		case 9: 
+			monthStatement = monthStatement + "September";
+			break;
+		case 10:
+			monthStatement = monthStatement + "October";
+			break;
+		case 11:
+			monthStatement = monthStatement + "November";
+			break;
+		case 12:
+			monthStatement = monthStatement + "December";
+			break;
+	    	default:
+	        	monthStatement = "";
+		} 
+
+	if (monthStatement != "")
+		returnStatement = returnStatement + " " + monthStatement;
 
         return returnStatement;
     }
